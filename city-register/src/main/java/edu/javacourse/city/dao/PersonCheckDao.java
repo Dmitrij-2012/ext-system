a4e0d38afea7c5ed6abe6899cd5f4a15cf61351d
Dmitry <dmitrij-2012@ukr.net>
2024-06-16 21:05:00 +0300

    87

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -1,6 +1,7 @@
 package edu.javacourse.city.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,14 +12,51 @@ import edu.javacourse.city.exception.PersonCheckException;
 
 public class PersonCheckDao {
 	
-	private static final String SQL_REQUEST = "";
-
+	 private static final String SQL_REQUEST =
+			 "select temporal from cr_address_person ap " +
+	                    "inner join cr_person p on p.person_id = ap.person_id " +
+	                    "inner join cr_address a on a.address_id = ap.address_id " +
+	                    "where " +
+	                    "CURRENT_DATE >= ap.start_date and (CURRENT_DATE <= ap.end_date or ap.end_date is null)" +
+	                    "and upper(p.sur_name COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+	                    "and upper(p.given_name COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+	                    "and upper(patronymic COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  " +
+	                    "and p.date_of_birth = ? " +
+	                    "and a.street_code = ?  " +
+	                    "and upper(a.building COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  ";
+	 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
+        
+        String sql = SQL_REQUEST;
+        if (request.getExtension() != null) {
+            sql += "and upper(a.extension COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\")  ";
+        } else {
+            sql += "and extension is null ";
+        }
+        if (request.getApartment() != null) {
+            sql += "and upper(a.apartment COLLATE \"en_US.UTF-8\") = upper(? COLLATE \"en_US.UTF-8\") ";
+        } else {
+            sql += "and a.apartment is null ";
+        }
 
         try (Connection con = getConnection();
-             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)) {
+        		 PreparedStatement stmt = con.prepareStatement(sql)) {
 
+            int count = 1;
+            stmt.setString(count++, request.getSurName());
+            stmt.setString(count++, request.getGivenName());
+            stmt.setString(count++, request.getPatronymic());
+            stmt.setDate(count++, java.sql.Date.valueOf(request.getDateOfBirth()));
+            stmt.setInt(count++, request.getStreetCode());
+            stmt.setString(count++, request.getBuilding());
+            if (request.getExtension() != null) {
+                stmt.setString(count++, request.getExtension());
+            }
+            if (request.getApartment() != null) {
+                stmt.setString(count++, request.getApartment());
+            }
+            
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
                 response.setRegistered(true);
@@ -30,9 +68,10 @@ public class PersonCheckDao {
 
         return response;
     }
-
-    private Connection getConnection() {
-        return null;
+    
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:postgresql://localhost/city_register",
+                "postgres", "123");
     }
 
 }
